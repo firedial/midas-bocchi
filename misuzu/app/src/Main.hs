7e7ba81c5bb175d3df579c6a8009b0a000f28519
firedial <42674772+firedial@users.noreply.github.com>
2023-04-05 22:32:25 +0900

    エコーサーバの作成

--- misuzu/app/src/Main.hs
+++ misuzu/app/src/Main.hs
@@ -1,5 +1,32 @@
-module Main (main) where
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Network.Wai (responseLBS, Application, getRequestBodyChunk)
+import Network.HTTP.Types (status200)
+import Network.Wai.Handler.Warp (run, Port)
+import System.Environment (getEnvironment)
+import Data.List (lookup)
+import Data.Maybe
+import Data.ByteString.Lazy (fromStrict)
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  port <- getPort
+  putStr "start Server: http://localhost:"
+  print port
+  run port helloApp
+
+helloApp :: Application
+helloApp req respond = ioResponse >>= respond
+  where
+    json = getRequestBodyChunk req
+    trans = responseLBS status200 []
+    ioResponse = fmap (trans . fromStrict) json
+
+getPort :: IO Port
+getPort = getEnvironment >>= return . port
+  where
+    port = fromMaybe defaultPort . fmap read . lookup "PORT"
+
+defaultPort :: Port
+defaultPort = 3000
