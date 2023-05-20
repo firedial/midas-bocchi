1a1fdfd2fc8eaef7c9a1edc9e3e9378670aadcb6
firedial <42674772+firedial@users.noreply.github.com>
2023-05-20 11:29:07 +0900

    名前の変更

--- haruhi/resources/js/components/HeaderComponent.vue
+++ haruhi/resources/js/components/HeaderComponent.vue
@@ -2,24 +2,24 @@
     <div class="container-fluid bg-dark mb-3">
         <div class="container">
             <nav class="navbar navbar-dark">
-                <span class="navbar-brand mb-0 h1">Midas Tari</span>
+                <span class="navbar-brand mb-0 h1">Midas Bocchi</span>
                 <div>
-                    <router-link v-bind:to="{name: 'balance.list'}">
+                    <router-link v-bind:to="{ name: 'balance.list' }">
                         <button class="btn btn-secondary">Balance List</button>
                     </router-link>
-                    <router-link v-bind:to="{name: 'balance.create'}">
+                    <router-link v-bind:to="{ name: 'balance.create' }">
                         <button class="btn btn-secondary">Balance Add</button>
                     </router-link>
-                    <router-link v-bind:to="{name: 'move.list', params: {attributeName: 'purposes'}}">
+                    <router-link v-bind:to="{ name: 'move.list', params: { attributeName: 'purposes' } }">
                         <button class="btn btn-success">Purpose Move List</button>
                     </router-link>
-                    <router-link v-bind:to="{name: 'move.create', params: {attributeName: 'purposes'}}">
+                    <router-link v-bind:to="{ name: 'move.create', params: { attributeName: 'purposes' } }">
                         <button class="btn btn-success">Purpose Move Add</button>
                     </router-link>
-                    <router-link v-bind:to="{name: 'move.list', params: {attributeName: 'places'}}">
+                    <router-link v-bind:to="{ name: 'move.list', params: { attributeName: 'places' } }">
                         <button class="btn btn-success">Place Move List</button>
                     </router-link>
-                    <router-link v-bind:to="{name: 'move.create', params: {attributeName: 'places'}}">
+                    <router-link v-bind:to="{ name: 'move.create', params: { attributeName: 'places' } }">
                         <button class="btn btn-success">Place Move Add</button>
                     </router-link>
                 </div>
@@ -29,6 +29,6 @@
 </template>
 
 <script>
-    export default {}
+export default {}
 </script>
 
