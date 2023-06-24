8c9b3bd593f80b3f65138be2bec832109c3ed73b
firedial <42674772+firedial@users.noreply.github.com>
2023-06-24 14:33:30 +0900

    category に対してバリデーションチェック追加

--- haruhi/app/Http/Controllers/AttributeCategoryController.php
+++ haruhi/app/Http/Controllers/AttributeCategoryController.php
@@ -6,22 +6,43 @@ use Illuminate\Http\Request;
 use App\Models\Balance;
 use App\Models\KindElement;
 use App\Service\AttributeCategoryService;
+use App\Exceptions\InvalidParameterException;
 
 class AttributeCategoryController extends Controller
 {
     public function index(String $attributeName)
     {
         $attributeCategoryService = new AttributeCategoryService();
+
+        if (!in_array($attributeName, ['kind_category', 'purpose_category', 'place_category'])) {
+            throw new InvalidParameterException("Wrong attribute name {$attributeName}.");
+        }
+
         return $attributeCategoryService->getAttributeCategories(['attributeName' => $attributeName]);
     }
 
     public function store(Request $request, String $attributeName)
     {
         $attributeCategoryService = new AttributeCategoryService();
+
+        if (!in_array($attributeName, ['kind_category', 'purpose_category', 'place_category'])) {
+            throw new InvalidParameterException("Wrong attribute name {$attributeName}.");
+        }
+
+        $name = $request->input('name');
+        if (!preg_match('/^[A-Z]\w*$/', $name) || strlen($name) > 20) {
+            throw new InvalidParameterException("Wrong name {$name}.");
+        }
+
+        $description = $request->input('description');
+        if (empty($description) || mb_strlen($description) > 20) {
+            throw new InvalidParameterException("Wrong description {$description}.");
+        }
+
         return $attributeCategoryService->createAttributeCategory([
             'attributeName' => $attributeName,
             'attributeCategory' => [
-                'name' => $request->input('name'),
+                'name' => $name,
                 'description' => $request->input('description'),
             ],
         ]);
@@ -30,6 +51,25 @@ class AttributeCategoryController extends Controller
     public function update(Request $request, String $attributeName, int $categoryId)
     {
         $attributeCategoryService = new AttributeCategoryService();
+
+        if (!in_array($attributeName, ['kind_category', 'purpose_category', 'place_category'])) {
+            throw new InvalidParameterException("Wrong attribute name {$attributeName}.");
+        }
+
+        if (empty($categoryId)) {
+            throw new InvalidParameterException("categoryId is empty.");
+        }
+
+        $name = $request->input('name');
+        if (!preg_match('/^[A-Z]\w*$/', $name) || strlen($name) > 20) {
+            throw new InvalidParameterException("Wrong name {$name}.");
+        }
+
+        $description = $request->input('description');
+        if (empty($description) || mb_strlen($description) > 20) {
+            throw new InvalidParameterException("Wrong description {$description}.");
+        }
+
         return $attributeCategoryService->updateAttributeCategory([
             'attributeName' => $attributeName,
             'attributeCategory' => [
