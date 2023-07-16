b52837465d7f1dfab7b575ec07574bf82db2d73f
firedial <42674772+firedial@users.noreply.github.com>
2023-07-16 16:30:20 +0900

    バックアップボタンにデプロイモードの追加

--- backup.py
+++ backup.py
@@ -18,8 +18,76 @@ GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # スイッチに接続
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+DOCKER_CONTEINER_RIKKA_NAME = 'midas-bocchi_rikka_1'
+
 # バックアップコマンド
-command = ['docker', 'exec', 'midas-bocchi_rikka_1', '/home/root/backup/main.sh']
+command = ['docker', 'exec', DOCKER_CONTEINER_RIKKA_NAME, '/home/root/backup/main.sh']
+
+
+def deploy():
+    # ボタン押された時
+    # 一旦両方つける
+    GPIO.output(25, GPIO.HIGH)
+    GPIO.output(24, GPIO.HIGH)
+
+    # イメージのプル
+    dockerPullCommand = ['docker', 'pull', 'firedial/midas-bocchi-haruhi']
+    cp = subprocess.run(dockerPullCommand)
+
+    if int(cp.returncode) != 0:
+        # 失敗した時は赤をつける
+        GPIO.output(25, GPIO.LOW)
+        GPIO.output(24, GPIO.HIGH)
+
+        # 1秒間点灯させて消す
+        sleep(1)
+        GPIO.output(25, GPIO.LOW)
+        GPIO.output(24, GPIO.LOW)
+        return
+
+    # コンテナに反映
+    dockerCommand = ['docker-compose', '-f', 'docker-compose-product.yml', 'up', '-d']
+    cp = subprocess.run(dockerCommand)
+
+    if int(cp.returncode) == 0:
+        # 成功した時は緑をつける
+        GPIO.output(25, GPIO.HIGH)
+        GPIO.output(24, GPIO.LOW)
+        # subprocess.run(['docker', 'exec', DOCKER_CONTEINER_RIKKA_NAME, '/home/root/button/notify.sh', 'deploy success!'])
+    else:
+        # 失敗した時は赤をつける
+        GPIO.output(25, GPIO.LOW)
+        GPIO.output(24, GPIO.HIGH)
+        # subprocess.run(['docker', 'exec', DOCKER_CONTEINER_RIKKA_NAME, '/home/root/button/notify.sh', 'deploy fail!'])
+
+    # 1秒間点灯させて消す
+    sleep(1)
+    GPIO.output(25, GPIO.LOW)
+    GPIO.output(24, GPIO.LOW)
+
+
+def backup():
+    # ボタン押された時
+    # 一旦両方つける
+    GPIO.output(25, GPIO.HIGH)
+    GPIO.output(24, GPIO.HIGH)
+
+    # バックアップコマンド実行
+    cp = subprocess.run(command)
+
+    if int(cp.returncode) == 0:
+        # 成功した時は緑をつける
+        GPIO.output(25, GPIO.HIGH)
+        GPIO.output(24, GPIO.LOW)
+    else:
+        # 失敗した時は赤をつける
+        GPIO.output(25, GPIO.LOW)
+        GPIO.output(24, GPIO.HIGH)
+
+    # 1秒間点灯させて消す
+    sleep(1)
+    GPIO.output(25, GPIO.LOW)
+    GPIO.output(24, GPIO.LOW)
 
 flashingCount = 0
 try:
@@ -39,55 +107,15 @@ try:
             if flashingCount >= 100:
                 flashingCount = 0
 
+            # ボタン押されたらデプロイする
             if GPIO.input(27) == GPIO.HIGH:
-                # ボタン押された時
-                # 一旦両方つける
-                GPIO.output(25, GPIO.HIGH)
-                GPIO.output(24, GPIO.HIGH)
+                deploy()
 
-                # イメージのプル
-                dockerPullCommand = ['docker', 'pull', 'firedial/midas-bocchi-haruhi']
-                cp = subprocess.run(dockerPullCommand)
 
-                # コンテナに反映
-                dockerCommand = ['docker-compose', '-f', 'docker-compose-product.yml', 'up', '-d']
-                cp = subprocess.run(dockerCommand)
-
-                if int(cp.returncode) == 0:
-                    # 成功した時は緑をつける
-                    GPIO.output(25, GPIO.HIGH)
-                    GPIO.output(24, GPIO.LOW)
-                else:
-                    # 失敗した時は赤をつける
-                    GPIO.output(25, GPIO.LOW)
-                    GPIO.output(24, GPIO.HIGH)
-
-                # 1秒間点灯させて消す
-                sleep(1)
-                GPIO.output(25, GPIO.LOW)
-                GPIO.output(24, GPIO.LOW)
-        elif GPIO.input(27) == GPIO.HIGH:
-            # ボタン押された時
-            # 一旦両方つける
-            GPIO.output(25, GPIO.HIGH)
-            GPIO.output(24, GPIO.HIGH)
-
-            # バックアップコマンド実行
-            cp = subprocess.run(command)
-
-            if int(cp.returncode) == 0:
-                # 成功した時は緑をつける
-                GPIO.output(25, GPIO.HIGH)
-                GPIO.output(24, GPIO.LOW)
-            else:
-                # 失敗した時は赤をつける
-                GPIO.output(25, GPIO.LOW)
-                GPIO.output(24, GPIO.HIGH)
-
-            # 1秒間点灯させて消す
-            sleep(1)
-            GPIO.output(25, GPIO.LOW)
-            GPIO.output(24, GPIO.LOW)
+        else:
+            # バックアップモード
+            if GPIO.input(27) == GPIO.HIGH:
+                backup()
 
         sleep(0.01)
 
