c479e68e63b9f53125f872eed98d983d3f671878
firedial <42674772+firedial@users.noreply.github.com>
2024-11-24 18:10:34 +0900

    404 にしていたところを戻した

--- haruhi/routes/web.php
+++ haruhi/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::fallback(function () {
-    return response()->make("", 404);
-});
+Route::get('/{any}', function() {
+        return view('app');
+    })->where('any', '.*');
