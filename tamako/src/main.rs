6a95b5ab42e18f4e25496845c7ab4dae1fe85b53
firedial <42674772+firedial@users.noreply.github.com>
2024-12-08 16:19:09 +0900

    取得まで実装

--- tamako/src/main.rs
+++ tamako/src/main.rs
@@ -1,3 +1,23 @@
+use curl::easy::Easy;
+use rustc_serialize::json::Json;
+
 fn main() {
-    println!("Hello, world!");
+    get("http://haruhi/api/balances");
+}
+
+fn get(url: &str) -> () {
+    let mut data = Vec::new();
+    let mut handle = Easy::new();
+    handle.url(url).unwrap();
+    {
+        let mut transfer = handle.transfer();
+        transfer.write_function(|new_data| {
+            data.extend_from_slice(new_data);
+            Ok(new_data.len())
+        }).unwrap();
+        transfer.perform().unwrap();
+    }
+    let r: String = String::from_utf8(data).unwrap();
+    println!("{}", r);
+    println!("{}", Json::from_str(&r).unwrap().find("message").unwrap());
 }
