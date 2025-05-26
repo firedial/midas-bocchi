5e8d6bb12fdf2b18e7cd3236ce86f08b5023d4e2
firedial <42674772+firedial@users.noreply.github.com>
2025-05-26 16:30:59 +0900

    内部で合計金額を計算するようにした

--- haruhi/app/Service/CheckPlaceSumService.php
+++ haruhi/app/Service/CheckPlaceSumService.php
@@ -4,6 +4,8 @@ namespace App\Service;
 
 use App\Models\Dao\CheckPlaceSumDao;
 use App\Models\Dao\Impl\CheckPlaceSumDaoImpl;
+use App\Models\Dao\BalanceDao;
+use App\Models\Dao\Impl\BalanceDaoImpl;
 
 /**
  * 金額確認のサービスクラス
@@ -12,17 +14,20 @@ class CheckPlaceSumService
 {
     private $checkPlaceSumDao;
 
-    public function __construct(CheckPlaceDao $checkPlaceSumDao = null)
+    public function __construct(CheckPlaceDao $checkPlaceSumDao = null, BalanceDao $balanceDao = null)
     {
         $this->checkPlaceSumDao = $checkPlaceSumDao ?: new CheckPlaceSumDaoImpl();
+        $this->balanceDao = $balanceDao ?: new BalanceDaoImpl();
     }
 
     public function registerCheckPlaceSum(array $data): bool
     {
         \DB::beginTransaction();
         try {
+            $sum = $this->balanceDao->getSum($data['placeElementId']);
+
             $value = [
-                'sum' => $data['sum'],
+                'sum' => $sum,
                 'place_element_id' => $data['placeElementId'],
                 'date' => $data['date'],
             ];
