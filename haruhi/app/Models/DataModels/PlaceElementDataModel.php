9ae4e3d5893e81cb16e08d89b2b85078260720f3
firedial <42674772+firedial@users.noreply.github.com>
2025-06-20 23:08:51 +0900

    定数化

---
+++ haruhi/app/Models/DataModels/PlaceElementDataModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\DataModels;
+
+use Illuminate\Support\Facades\DB;
+
+class PlaceElementDataModel
+{
+    public const TABLE_NAME = "m_place_element";
+
+    public const C_ID = "id";
+    public const C_NAME = "name";
+    public const C_DESCRIPTION = "description";
+    public const C_PRIORITY = "priority";
+    public const C_CATEGORY_ID = "category_id";
+}
