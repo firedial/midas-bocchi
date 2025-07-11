53df5673867dce558004115482f218afb1ea20ba
firedial <42674772+firedial@users.noreply.github.com>
2025-07-11 23:43:42 +0900

    azusa の修正

---
+++ azusa/initdb.d/102_dml.sql
@@ -0,0 +1,6 @@
+INSERT INTO m_kind_category (id, name, description) VALUES (1, "move", "移動");
+INSERT INTO m_purpose_category (id, name, description) VALUES (1, "move", "移動");
+INSERT INTO m_place_category (id, name, description) VALUES (1, "move", "移動");
+INSERT INTO m_kind_element (id, name, description, priority, category_id) VALUES (1, "move", "移動", 1, 1);
+INSERT INTO m_purpose_element (id, name, description, priority, category_id) VALUES (1, "move", "移動", 1, 1);
+INSERT INTO m_place_element (id, name, description, priority, category_id) VALUES (1, "move", "移動", 1, 1);
