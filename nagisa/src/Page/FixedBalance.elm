75dcbe3901f4cbe96feea8b266f59e0e8dbccfd1
firedial <42674772+firedial@users.noreply.github.com>
2025-08-09 14:36:38 +0900

    日付と金額が入力できるようにした

--- nagisa/src/Page/FixedBalance.elm
+++ nagisa/src/Page/FixedBalance.elm
@@ -2,6 +2,7 @@ module Page.FixedBalance exposing (Model, Msg, init, update, view)
 
 import Html
 import Html.Attributes as Attributes
+import Html.Events exposing (onInput)
 import List
 import Maybe
 import Model.Enitity.FixedBalanceEntity as FixedBalanceEntity
@@ -11,13 +12,29 @@ import String
 
 
 type alias Model =
-    { fixedBalances : FixedBalanceEntity.FixedBalances
+    { stringBalances : List StringBalance
     , errorMessage : Maybe String
+    }
+
+
+type alias StringBalance =
+    { fixedBalanceId : String
+    , amount : String
+    , item : String
+    , kindElementId : String
+    , purposeElementId : String
+    , placeElementId : String
+    , kindElementDescription : String
+    , purposeElementDescription : String
+    , placeElementDescription : String
+    , date : String
     }
 
 
 type Msg
     = GetFixedBalances (Result Request.Error FixedBalanceEntity.FixedBalances)
+    | InputAmount String String
+    | InputDate String String
 
 
 init : ( Model, Cmd Msg )
@@ -33,13 +50,61 @@ update msg model =
         GetFixedBalances result ->
             case result of
                 Ok response ->
-                    ( { model | fixedBalances = response }, Cmd.none )
+                    let
+                        stringBalances =
+                            List.map
+                                (\fixedBalance ->
+                                    StringBalance
+                                        (String.fromInt fixedBalance.fixedBalanceId)
+                                        (String.fromInt fixedBalance.amount)
+                                        fixedBalance.item
+                                        (String.fromInt fixedBalance.kindElementId)
+                                        (String.fromInt fixedBalance.purposeElementId)
+                                        (String.fromInt fixedBalance.placeElementId)
+                                        fixedBalance.kindElementDescription
+                                        fixedBalance.purposeElementDescription
+                                        fixedBalance.placeElementDescription
+                                        ""
+                                )
+                                response
+                    in
+                    ( { model | stringBalances = stringBalances }, Cmd.none )
 
                 Err (Request.DecodeError message) ->
                     ( { model | errorMessage = Just message }, Cmd.none )
 
                 Err (Request.RequestError message) ->
                     ( { model | errorMessage = Just message }, Cmd.none )
+
+        InputAmount fixedBalanceId amount ->
+            let
+                newStringBalances =
+                    List.map
+                        (\stringBalance ->
+                            if stringBalance.fixedBalanceId == fixedBalanceId then
+                                { stringBalance | amount = amount }
+
+                            else
+                                stringBalance
+                        )
+                        model.stringBalances
+            in
+            ( { model | stringBalances = newStringBalances }, Cmd.none )
+
+        InputDate fixedBalanceId date ->
+            let
+                newStringBalances =
+                    List.map
+                        (\stringBalance ->
+                            if stringBalance.fixedBalanceId == fixedBalanceId then
+                                { stringBalance | date = date }
+
+                            else
+                                stringBalance
+                        )
+                        model.stringBalances
+            in
+            ( { model | stringBalances = newStringBalances }, Cmd.none )
 
 
 view : Model -> Html.Html Msg
@@ -50,11 +115,12 @@ view model =
             (Html.tr
                 []
                 [ Html.th [] [ Html.text "id" ]
-                , Html.th [] [ Html.text "金額" ]
                 , Html.th [] [ Html.text "項目" ]
                 , Html.th [] [ Html.text "種別" ]
                 , Html.th [] [ Html.text "予算" ]
                 , Html.th [] [ Html.text "場所" ]
+                , Html.th [] [ Html.text "金額" ]
+                , Html.th [] [ Html.text "日付" ]
                 ]
                 :: Html.tr
                     []
@@ -64,18 +130,20 @@ view model =
                     , Html.td [] [ Html.text "" ]
                     , Html.td [] [ Html.text "" ]
                     , Html.td [] [ Html.text "" ]
+                    , Html.td [] [ Html.text "" ]
                     ]
                 :: List.map
-                    (\fixedBalance ->
+                    (\stringBalance ->
                         Html.tr []
-                            [ Html.td [] [ Html.a [ Attributes.href <| Route.toPath (Route.FixedBalanceId fixedBalance.fixedBalanceId) ] [ Html.text <| String.fromInt fixedBalance.fixedBalanceId ] ]
-                            , Html.td [] [ Html.text <| String.fromInt fixedBalance.amount ]
-                            , Html.td [] [ Html.text fixedBalance.item ]
-                            , Html.td [] [ Html.text fixedBalance.kindElementDescription ]
-                            , Html.td [] [ Html.text fixedBalance.purposeElementDescription ]
-                            , Html.td [] [ Html.text fixedBalance.placeElementDescription ]
+                            [ Html.td [] [ Html.a [ Attributes.href <| Route.toPath (String.toInt stringBalance.fixedBalanceId |> Maybe.withDefault 0 |> Route.FixedBalanceId) ] [ Html.text stringBalance.fixedBalanceId ] ]
+                            , Html.td [] [ Html.text stringBalance.item ]
+                            , Html.td [] [ Html.text stringBalance.kindElementDescription ]
+                            , Html.td [] [ Html.text stringBalance.purposeElementDescription ]
+                            , Html.td [] [ Html.text stringBalance.placeElementDescription ]
+                            , Html.td [] [ Html.input [ Attributes.type_ "text", Attributes.value stringBalance.amount, onInput (InputAmount stringBalance.fixedBalanceId) ] [] ]
+                            , Html.td [] [ Html.input [ Attributes.type_ "date", Attributes.value stringBalance.date, onInput (InputDate stringBalance.fixedBalanceId) ] [] ]
                             ]
                     )
-                    model.fixedBalances
+                    model.stringBalances
             )
         ]
