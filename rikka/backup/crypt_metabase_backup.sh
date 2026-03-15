60ee8d2ad0e9c2b029450d1c4c711f906b1af83b
firedial <42674772+firedial@users.noreply.github.com>
2026-03-15 16:20:33 +0900

    mariadb に変えた

--- rikka/backup/crypt_metabase_backup.sh
+++ rikka/backup/crypt_metabase_backup.sh
@@ -3,7 +3,7 @@
 BACKUP_FILE_NAME="backup.dump"
 BACKUP_DIR="/mnt/nas/bocchi/metabase"
 
-/usr/bin/mysqldump --no-tablespaces --single-transaction -u ${DB_TASK_USER} -p${DB_TASK_PASS} -h ${DB_HOST} ${DB_METABASE_DATABASE_NAME} > /tmp/${BACKUP_FILE_NAME}
+/usr/bin/mariadb-dump --no-tablespaces --single-transaction -u ${DB_TASK_USER} -p${DB_TASK_PASS} -h ${DB_HOST} ${DB_METABASE_DATABASE_NAME} > /tmp/${BACKUP_FILE_NAME}
 gzip /tmp/${BACKUP_FILE_NAME}
 BACKUP_GZIP_FILE_NAME=${BACKUP_FILE_NAME}.gz
 
