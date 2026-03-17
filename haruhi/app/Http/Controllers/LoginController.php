44a7b56c3c944d9442b312408828f16b007b5ab9
firedial <42674772+firedial@users.noreply.github.com>
2026-03-17 22:43:48 +0900

    いつの間にか消えていた

--- haruhi/app/Http/Controllers/LoginController.php
+++ haruhi/app/Http/Controllers/LoginController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Exceptions\InvalidParameterException;
+use Exception;
 
 class LoginController extends Controller
 {
