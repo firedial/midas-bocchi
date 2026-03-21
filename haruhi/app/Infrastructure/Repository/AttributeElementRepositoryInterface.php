1b7abb759327ae25c6f3ad587bf3f53de86b5c16
firedial <42674772+firedial@users.noreply.github.com>
2026-03-21 11:42:23 +0900

    element について修正

--- haruhi/app/Infrastructure/Repository/AttributeElementRepositoryInterface.php
+++ haruhi/app/Infrastructure/Repository/AttributeElementRepositoryInterface.php
@@ -10,6 +10,6 @@ interface AttributeElementRepositoryInterface
 {
     public function getAttributeElements(Attribute $attribute, ?AttributeElementId $attributeElementId): array;
     public function selectAttributeElement(Attribute $attribute, AttributeElementId $attributeElementId): ?AttributeElementEntity;
-    public function insertAttributeElement(AttributeElementEntity $attributeElement): int;
-    public function updateAttributeElement(AttributeElementEntity $attributeElement): void;
+    public function insertAttributeElement(AttributeElementEntity $attributeElement): AttributeElementEntity;
+    public function updateAttributeElement(AttributeElementEntity $attributeElement): AttributeElementEntity;
 }
